147ef586cf9a61ffc066d06fd651ddf18afff75a
christina <andrea.christina.unger@gmail.com>
2023-01-03 08:14:06 +0100

    p3

--- project-euler/fortran/problem3.f90
+++ project-euler/fortran/problem3.f90
@@ -15,5 +15,45 @@ program problem3
   num = 600851475143_LONG
 
   print *, num
-  print *, huge(num) ! 9223372036854775807
+  print *, largest_prime_factor_of(num)
+
+  ! ---- Tests ----
+
+  write (*,*) largest_prime_factor_of(2_LONG) == 2_LONG
+  write (*,*) largest_prime_factor_of(4_LONG) == 2_LONG
+  write (*,*) largest_prime_factor_of(10_LONG) == 5_LONG
+  write (*,*) largest_prime_factor_of(13195_LONG) == 29_LONG
+
+contains
+
+  function largest_prime_factor_of(num) result (factor)
+    implicit none
+
+    integer(kind=LONG), intent(in) :: num
+    integer(kind=LONG) :: factor
+
+    integer(kind=LONG) :: n
+    integer(kind=LONG) :: i
+    integer(kind=LONG) :: upper_bound
+
+    n = num
+    factor = num
+    upper_bound = floor(sqrt(real(n, LONG)))
+
+    do while (mod(n, 2_LONG) == 0)
+      n = n / 2_LONG
+      factor = 2_LONG
+    end do
+
+    do i = 3_LONG, upper_bound, 2
+      do while (mod(n, i) == 0)
+        n = n/i
+        factor = i
+      end do
+    end do
+
+    if (n > 2) then
+      factor = n
+    end if
+  end function largest_prime_factor_of
 end program problem3
